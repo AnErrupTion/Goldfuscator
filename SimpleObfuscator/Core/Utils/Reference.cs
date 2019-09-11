fe36c63035932d8dafba34c3bc4d04cd8544f0fe
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-09-11 20:27:14 +0200

    1.0

---
+++ SimpleObfuscator/Core/Utils/Reference.cs
@@ -0,0 +1,9 @@
+﻿namespace Goldfuscator.Core.Utils
+{
+    public class Reference
+    {
+        public static string Name = "Goldfuscator";
+        public static string Prefix = "[" + Name.ToUpper() + "] ";
+        public static string Version = "1.0";
+    }
+}
