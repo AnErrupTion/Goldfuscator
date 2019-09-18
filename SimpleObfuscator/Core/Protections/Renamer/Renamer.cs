47f42fa40331c47811fc079b877cc8edf06889be
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-09-18 18:56:33 +0200

    1.4

--- SimpleObfuscator/Core/Protections/Renamer/Renamer.cs
+++ SimpleObfuscator/Core/Protections/Renamer/Renamer.cs
@@ -16,24 +16,24 @@ namespace Goldfuscator.Core.Protections
 			{
 				if (CanRename(type) && !Program.IsWinForms && !Program.FileExtension.Contains("dll") && !module.HasResources)
                 {
-                    Console.WriteLine("  [RENAMER] Renaming Type Name \"" + type.Name + "\"...");
-                    type.Name = GenerateRandomString(50);
-                    Console.WriteLine("  [RENAMER] Renaming Type Namespace \"" + type.Namespace + "\"...");
-                    type.Namespace = GenerateRandomString(50);
+                    Console.WriteLine($"  [RENAMER] Renaming Type Name \"{type.Name}\"...");
+                    type.Name = GenerateRandomString(Next(50, 70));
+                    Console.WriteLine($"  [RENAMER] Renaming Type Namespace \"{type.Namespace}\"...");
+                    type.Namespace = GenerateRandomString(Next(50, 70));
                 }
 
                 foreach (var m in type.Methods)
                 {
                     if (CanRename(m) && !Program.IsWinForms && !Program.FileExtension.Contains("dll"))
                     {
-                        Console.WriteLine("  [RENAMER] Renaming method \"" + m.Name + "\"...");
-                        m.Name = GenerateRandomString(50);
+                        Console.WriteLine($"  [RENAMER] Renaming method \"{m.Name}\"...");
+                        m.Name = GenerateRandomString(Next(50, 70));
                     }
 
                     foreach (var para in m.Parameters)
                     {
-                        Console.WriteLine("  [RENAMER] Renaming method \"" + m.Name + "\"'s parameter \"" + para.Name + "\"...");
-                        para.Name = GenerateRandomString(50);
+                        Console.WriteLine($"  [RENAMER] Renaming method \"{m.Name}\"'s parameter \"{para.Name}\"...");
+                        para.Name = GenerateRandomString(Next(50, 70));
                     }
                 }
 
@@ -41,8 +41,8 @@ namespace Goldfuscator.Core.Protections
                 {
                     if (CanRename(p))
                     {
-                        Console.WriteLine("  [RENAMER] Renaming property \"" + p.Name + "\"...");
-                        p.Name = GenerateRandomString(50);
+                        Console.WriteLine($"  [RENAMER] Renaming property \"{p.Name}\"...");
+                        p.Name = GenerateRandomString(Next(50, 70));
                     }
                 }
 
@@ -50,8 +50,8 @@ namespace Goldfuscator.Core.Protections
                 {
                     if (CanRename(field))
                     {
-                        Console.WriteLine("  [RENAMER] Renaming field \"" + field.Name + "\"...");
-                        field.Name = GenerateRandomString(50);
+                        Console.WriteLine($"  [RENAMER] Renaming field \"{field.Name}\"...");
+                        field.Name = GenerateRandomString(Next(50, 70));
                     } 
                 }
 
@@ -59,8 +59,8 @@ namespace Goldfuscator.Core.Protections
                 {
                     if (CanRename(e))
                     {
-                        Console.WriteLine("  [RENAMER] Renaming event \"" + e.Name + "\"...");
-                        e.Name = GenerateRandomString(50);
+                        Console.WriteLine($"  [RENAMER] Renaming event \"{e.Name}\"...");
+                        e.Name = GenerateRandomString(Next(50, 70));
                     }
                 }
             }
