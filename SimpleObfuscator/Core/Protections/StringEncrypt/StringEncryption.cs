6a423e44a2e37d9bd5b3f0f1bc4c39f7827a97fe
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-09-18 18:57:08 +0200

    Update StringEncryption.cs

--- SimpleObfuscator/Core/Protections/StringEncrypt/StringEncryption.cs
+++ SimpleObfuscator/Core/Protections/StringEncrypt/StringEncryption.cs
@@ -1,4 +1,4 @@
-﻿using dnlib.DotNet;
+using dnlib.DotNet;
 using System.Text;
 using System;
 using dnlib.DotNet.Emit;
@@ -12,27 +12,22 @@ namespace Goldfuscator.Core.Protections.StringEncrypt
         /// </summary>
         public static void Execute(ModuleDefMD module)
         {
-            foreach (var type in module.Types)
-            {
-                MethodDef cctor = module.GlobalType.FindOrCreateStaticConstructor();
-                MethodDef strings = CreateReturnMethodDef(Encoding.UTF8.GetString(Convert.FromBase64String("dGVzdA==")), cctor);
-                type.Methods.Add(strings);
-            }
+            MethodDef cctor = module.GlobalType.FindOrCreateStaticConstructor();
+            MethodDef strings = CreateReturnMethodDef(Encoding.UTF8.GetString(Convert.FromBase64String("dGVzdA==")), cctor);
+            Console.WriteLine($"  [STRINGENCRYPTION] Adding method \"{strings.Name}\" in \"{cctor.Name}\"...");
+            module.GlobalType.Methods.Add(strings);
         }
 
-        private static MethodDef CreateReturnMethodDef(string value, MethodDef source_method)
+        private static MethodDef CreateReturnMethodDef(string value, MethodDef sourceMethod)
         {
-            CorLibTypeSig corlib = source_method.Module.CorLibTypes.String;
-            MethodDef newMethod = new MethodDefUser("Goldfuscation",
-                    MethodSig.CreateStatic(corlib),
+            MethodDef newMethod = new MethodDefUser("Decrypt",
+                    MethodSig.CreateStatic(sourceMethod.Module.CorLibTypes.String),
                     MethodImplAttributes.IL | MethodImplAttributes.Managed,
                     MethodAttributes.Public | MethodAttributes.Static | MethodAttributes.HideBySig)
-            {
-                Body = new CilBody()
-            };
+            { Body = new CilBody() };
 
-            newMethod.Body.Instructions.Add(Instruction.Create(OpCodes.Ldstr, value));
-            newMethod.Body.Instructions.Add(new Instruction(OpCodes.Ret));
+            newMethod.Body.Instructions.Add(OpCodes.Ldstr.ToInstruction(value));
+            newMethod.Body.Instructions.Add(OpCodes.Ret.ToInstruction());
             return newMethod;
         }
     }
